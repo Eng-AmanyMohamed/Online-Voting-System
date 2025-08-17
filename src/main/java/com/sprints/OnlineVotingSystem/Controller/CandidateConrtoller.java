3250c1b46fe32af366a4f780a3e2ca6b1a292fcf
EsraaTarek382 <esraatareksamir@gmail.com>
2025-08-17 12:08:28 +0300

    Candidate Controller

---
+++ src/main/java/com/sprints/OnlineVotingSystem/Controller/CandidateConrtoller.java
@@ -0,0 +1,51 @@
+package com.sprints.OnlineVotingSystem.Controller;
+
+import com.sprints.OnlineVotingSystem.Service.Candidate.CandidateService;
+import com.sprints.OnlineVotingSystem.dto.request.CandidateDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/candidates")
+public class CandidateConrtoller {
+
+    private final CandidateService candidateService;
+
+    public CandidateConrtoller(CandidateService candidateService) {
+        this.candidateService = candidateService;
+    }
+
+    @PostMapping("/add")
+    @PreAuthorize("hasRole('Admin')")
+    public ResponseEntity<CandidateDTO> addCandidate(@Valid @RequestBody CandidateDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(candidateService.addCandidate(dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CandidateDTO> getCandidateById(@PathVariable Long id) {
+        return ResponseEntity.ok(candidateService.getCandidateById(id));
+    }
+
+    @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('Admin')")
+    public ResponseEntity<CandidateDTO> updateCandidate(@Valid @RequestBody CandidateDTO dto, @PathVariable Long id) {
+        return ResponseEntity.ok(candidateService.updateCandidate(dto, id));
+    }
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('Admin')")
+    public ResponseEntity<CandidateDTO> deleteCandidate(@PathVariable Long id) {
+        candidateService.deleteCandidate(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{election_id}")
+    public ResponseEntity<List<CandidateDTO>> getAllCandidatesPerElection(@PathVariable long election_id) {
+        return ResponseEntity.ok(candidateService.getAllCandidatesPerElection(election_id));
+    }
+
+}
